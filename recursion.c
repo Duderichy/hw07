155361ea0e3be631a49fb256e0e42b5880ce0e9b
Richard Bibeault <Richard@Richards-MacBook-Pro.local>
2016-08-08 11:24:29 -0400

    started writing

---
+++ recursion.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+void integral_recur (int nmin, int nmax, double vals[]);
+
+void integral_recur (int nmin, int nmax, double vals[])	{
+
+	vals[0] = 1. - 1/M_E // Sets the first value for the sum
+	
+	for( i = 1; i <= nmax; vals[i] = (double)i*vals[i]
+		
+}
+
+
+void integral_gen (int nmin, int nmax, double vals[]);
+
+void integral_gen (int nmin, int nmax, double vals[])	{
+
+
+
+}
+
+
+
+
+int main(void)	{
+
+	#DEFINE NMAX 100
+
+	double vals1[NMAX + 1], vals2[NMAX + 1];	
+
+	void integral_recur (int nmin, int nmax, double vals1[]);
+
+	void integral_gen (int nmin, int nmax, double vals2[]);
+}
