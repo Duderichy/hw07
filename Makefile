8a94a687c5175cb3b6018f67decacbedf4532dae
Richard Bibeault <Richard@Richards-MacBook-Pro.local>
2017-08-22 18:44:54 -0400

    project complete, but some documentation needed

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all: integrate.c integral_general.c
-	gcc -Wall -I/usr/local/include -c integral_general.c integrate.c
-	gcc -L/usr/local/lib -lgsl integrate.o integral_general.o -o integration.o
+	gcc -Wall -I/usr/local/include -c integral_general.c integrate.c adjust.c timer.c
+	gcc -L/usr/local/lib -lgsl integrate.o timer.o adjust.o integral_general.o -o integration.o
 
 clean :
 	rm -f *.o
--- integrate.c
+++ integrate.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 
+#include "timer.h"
+#include "adjust.h"
+
 #include "integral_general.h"
 
 void integral_recur (int nmin, int nmax, double vals[])	{
 
-	double vals1[100 + 1]; 
+	double vals1[100 + 1];
 
 	vals1[100] = 0.00367843028136748866811;
 
@@ -35,12 +38,76 @@ int main(void)	{
 	double vals1[NMAX + 1], vals2[NMAX + 1];
 
 	integral_recur (nminn, nmaxx, vals1);
-	
+
 	integral_gen (nminn, nmaxx, vals2);
 
 	for(int i = 0,j = nminn; i <= nmaxx - nminn; i++,j++)	{
 		printf("%.18f\t%.18f\t%d\n", vals1[i], vals2[i], j);
 	}
+
+	int count = 10;
+	double time;
+	double time1;
+	double tmin = 1;
+	double tmax = 2;
+
+	printf("integral_recur\n");
+
+	do {
+			timer_start ();
+
+			for (int k = 0; k <= count; k++) {
+
+				integral_recur (nminn, nmaxx, vals1);
+
+			}
+
+			time = timer_stop ();
+
+			time1 = time / count;
+
+			printf (" %10.2f usec     %10.6f sec    %10d\n",
+					time1 * 1.e6, time, count);
+
+			/*
+			 * adjust count such that cpu time is between
+			 * tmin and tmax
+			 */
+
+			count = adjust_rep_count (count, time, tmin, tmax);
+
+
+	} while ((time > tmax) || (time < tmin));
+
+	printf("integral_gen\n");
+
+	count = 10;
+
+	do {
+			timer_start ();
+
+			for (int k = 0; k <= count; k++) {
+
+			integral_gen (nminn, nmaxx, vals2);
+
+			}
+
+			time = timer_stop ();
+
+			time1 = time / count;
+
+			printf (" %10.2f usec     %10.6f sec    %10d\n",
+					time1 * 1.e6, time, count);
+
+			/*
+			 * adjust count such that cpu time is between
+			 * tmin and tmax
+			 */
+
+			count = adjust_rep_count (count, time, tmin, tmax);
+
+
+	} while ((time > tmax) || (time < tmin));
 
 	return 0;
 }
